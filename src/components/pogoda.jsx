eac7059f028843f8b6486f7f06a2ae5a1a74e0d2
kairat <63288004+Kairat77@users.noreply.github.com>
2023-01-02 14:56:28 +0600

    first commit

--- src/components/pogoda.jsx
+++ src/components/pogoda.jsx
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
 const Pogoda = () => {
-  const [forecast, setForecast] = useState(null);
-  const [city, setCity] = useState('Bishkek');
-  const [error, setError] = useState(null);
+  const [forecast, setForecast] = useState({list: []});
+  const [city, setCity] = useState('');
+  const [error, setError] = useState('');
   const apikey = "6aa7cd447bd496d76328849ac6af735a"
   
 
   useEffect(() => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`)
+    if (city.length){fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`)
       .then(response => response.json())
       .then(data => {
         setForecast(data);
       })
       .catch(error => {
         setError(error);
-      });
+      });}
   }, [city]);
+
 
   const handleCityChange = event => {
     setCity(event.target.value);
   };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (!forecast) {
-    return <div>Loading...</div>;
-  }
+  
 
   return (
     <div>
@@ -37,14 +32,18 @@ const Pogoda = () => {
         Location:
         <input type="text" value={city} onChange={handleCityChange} />
       </label>
-      {/* <button onClick={search}>Search</button> */}
-      {forecast.list.map(item => (
-        <div key={item.dt}>
-          <h2>{item.dt_txt}</h2>
-          <p>Temperature: {item.main.temp}°C</p>
-          <p>Description: {item.weather[0].description}</p>
-        </div>
-      ))}
+     
+      {forecast?.list ?
+         forecast.list.map(item => (
+          <div key={item.dt}>
+            <h2>{item.dt_txt}</h2>
+            <p>Temperature: {item.main.temp}°C</p>
+            <p>Description: {item.weather[0].description}</p>
+          </div>
+        ))
+        :
+        <div>нечего не найдено</div>
+    }
     </div>
   );
 };
